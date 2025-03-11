212415617350f5e34de75d1d864687dee8d88a80
weebo-ary <aryan.bhardwaj1233@gmail.com>
2025-03-11 14:16:45 +0530

    feat: add z-index to banner components for improved layout and visibility

--- src/pages/monitoring/Banner/EnergyBanner.js
+++ src/pages/monitoring/Banner/EnergyBanner.js
@@ -74,7 +74,7 @@ const BannerWithFadingGridBackground = () => {
           for School and College Students
         </h1>
       </div>
-      <div className="flex md:flex-row-reverse xs:flex-col-reverse sm:flex-col items-center justify-center gap-10">
+      <div className="flex md:flex-row-reverse xs:flex-col-reverse sm:flex-col items-center justify-center gap-10 z-10">
         <div className="md:w-1/3 sm:w-full xs:w-full flex flex-col md:flex-col sm:flex-row xs:flex-row justify-start items-center md:items-start md:justify-center space-y-4 md:space-y-0 md:space-x-4 md:gap-10 sm:gap-2 xs:gap-2">
           <p className="sm:text-start xs:text-start text-base md:text-4xl text-gray-800 dark:text-white mb-10 sm:mb-0 xs:mb-0">
             Discover More About FlixYoung Digital Creators
@@ -85,9 +85,9 @@ const BannerWithFadingGridBackground = () => {
             width="720"
             height="480"
             src="https://www.youtube.com/embed/kTdPSRB1t_Y?start=3"
-            title="Flix Start Digital"
+            title="FlixYoung Digital Creators"
             allowFullScreen
-            className="rounded-lg shadow-lg"
+            className="rounded-lg shadow-lg "
           ></iframe>
         </div>
       </div>
